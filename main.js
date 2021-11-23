f16477940cb3a67de5efafa1888d67ad354591e2
Guilherme Chehade <gui_chehade@hotmail.com>
2021-11-23 15:09:10 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,74 @@
+
+const Main = {
+    init: function () { 
+        // Initialize the todo list
+        // Inicializa a lista de tarefas
+        this.cacheSelectors();
+        this.bindEvents();
+    },
+    cacheSelectors: function () {
+        // Create variables to hold the selectors
+        // Cria variáveis para selecionar os elementos
+        this.$checkButtons = document.querySelectorAll('.check')
+        this.$inputTask = document.querySelector('#inputTask')
+        this.$list = document.querySelector('#list')
+        this.$removeButtons = document.querySelectorAll('.remove')
+    },
+    bindEvents: function () {
+        // Bind the events
+        // Vincula os eventos
+        this.$checkButtons.forEach(button => {
+            button.addEventListener('click', this.Events.checkButton_click)
+        })
+
+        this.$inputTask.onkeypress = this.Events.inputTask_keypress.bind(this)
+
+        this.$removeButtons.forEach(button => {
+            button.addEventListener('click', this.Events.removeButton_click)
+        })
+    },
+    Events: {
+        checkButton_click: function (event) {
+            const li = event.target.parentElement
+            const isDone = li.classList.contains('done')
+
+            if (!isDone) {
+                return li.classList.add('done')
+            }
+            li.classList.remove('done')
+        },
+
+        inputTask_keypress: function (event) {
+
+            const key = event.key
+            const value = event.target.value
+            if (key === 'Enter') {
+                this.$list.innerHTML += `
+                    <li>
+                        <div class="check"></div>
+                        <label class="task">
+                        ${value}
+                        </label>
+                        <button class="remove"></button>
+                    </li>
+                `
+
+                event.target.value = ''
+
+                this.cacheSelectors()
+                this.bindEvents()
+                console.log(this.$list)
+            }
+        },
+
+        removeButton_click: function (event) {
+            let li = event.target.parentElement
+            li.classList.add('removed')
+            setTimeout(() => {
+                li.classList.add('hidden')
+            }, 300)
+        }
+    }
+}
+
+Main.init();
